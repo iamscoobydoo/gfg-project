a9148c4b82a176ed2ebcdd8ae0c1c2adf220074e
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-05-15 21:29:00 +0530

    add auth, user, problem routes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//connect to DB
+connectDB();
+
+//Initialise Middleware
+app.use(express.json({ extended: false }));
+
+//define routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/user", require("./routes/api/user"));
+app.use("/api/problems", require("./routes/api/problems"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
