de445e6a6210e5a9ae73d87cd9aa40a9adf6ef9c
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-05-15 20:56:43 +0530

    add api.post route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,9 +9,9 @@ const User = require("../../models/User");
 
 const router = express.Router();
 
-//@route GET api/auth
-//@desc Get user details
-//@access Public
+//@route    GET api/auth
+//@desc     Get user details
+//@access   Public
 router.get("/", auth, async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select("-password");
@@ -21,3 +21,46 @@ router.get("/", auth, async (req, res) => {
         return res.status(500).send("Server Error");
     }
 });
+
+//@route    POST api/auth
+//@desc     Authenticate user and get token
+//@access   Public
+router.post(
+    "/",
+    [
+        check("email", "Please enter a valid email").isEmail(),
+        check("password", "Password is required").exists(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+        try {
+            let user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({ errors: [{ msg: "Invalid credentials" }] });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({ errors: [{ msg: "Invalid credentials" }] });
+            }
+
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+            jwt.sign(payload, process.env.JWT_SCERET, { expiresIn: 36000 }, (err, token) => {
+                if (err) throw err;
+                return res.json({ token });
+            });
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).send("Server Error");
+        }
+    }
+);
