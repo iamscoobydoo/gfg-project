528e70ebd8f4739a95b7e30c5cf99555278ec7b1
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-05-15 21:22:58 +0530

    add connectDB function

--- config/db.js
+++ config/db.js
@@ -1,2 +1,19 @@
 require("dotenv").config({ path: "../.env" });
 const mongoose = require(mongoose);
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.mongoURI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        });
+        console.log("Connected To DB!");
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1); //exit process on failure
+    }
+};
+
+module.exports = connectDB;
