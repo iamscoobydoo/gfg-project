3685c3d3acde97e628bdb7abbb5042ab484ce4ec
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-05-22 13:02:37 +0530

    fixed import

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: "./.env" });
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.mongoURI, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useCreateIndex: true,
             useUnifiedTopology: true,
