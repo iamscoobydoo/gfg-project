3c7e77121ca32add783c5cb771f3c7b200c79720
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-05-22 12:44:35 +0530

    add first router

--- routes/api/problems.js
+++ routes/api/problems.js
@@ -7,4 +7,15 @@ const router = express.Router();
 //@route    POST api/problems/all
 //@desc     Add all problems to DB
 //@access   me :)
-router.post("/all", async (req, res) => {});
+router.post("/all", async (req, res) => {
+    const { topic_name, problem_list } = req.body;
+    try {
+        problem = new Problem({ topic_name, problem_list });
+        await problem.save();
+    } catch (err) {
+        console.log(err.message);
+        return res.status(500).send("Server error");
+    }
+});
+
+module.exports = router;
