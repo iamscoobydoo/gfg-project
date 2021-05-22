2faf28eaf85a255765ddf5e7750b3b49cad91341
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-05-22 12:37:27 +0530

    init route to add all problems

--- routes/api/problems.js
+++ routes/api/problems.js
@@ -3,3 +3,8 @@ const express = require("express");
 const Problem = require("../../models/Problem");
 
 const router = express.Router();
+
+//@route    POST api/problems/all
+//@desc     Add all problems to DB
+//@access   me :)
+router.post("/all", async (req, res) => {});
