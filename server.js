b603a96e144f22e756c668698d3aa330b1c59afa
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-05-22 13:14:40 +0530

    fixed api import

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json({ extended: false }));
 //define routes
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/user", require("./routes/api/user"));
-app.use("/api/problems", require("./routes/api/problems"));
+app.use("/api/problems", require("./routes/api/problem"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
