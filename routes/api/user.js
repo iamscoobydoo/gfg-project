d7c30aa3620a692e1b2dceee7ff7cc3ecc241a2b
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-05-22 13:25:14 +0530

    fix an issue where the secret key was not read

--- routes/api/user.js
+++ routes/api/user.js
@@ -46,10 +46,15 @@ router.post(
                     id: user.id,
                 },
             };
-            jwt.sign(payload, process.env.JWT_SCERET, { expiresIn: 36000 }, (err, token) => {
-                if (err) throw err;
-                return res.json({ token });
-            });
+            jwt.sign(
+                payload,
+                `${process.env.JWT_SECRET_KEY}`,
+                { expiresIn: 36000 },
+                (err, token) => {
+                    if (err) throw err;
+                    return res.json({ token });
+                }
+            );
         } catch (err) {
             console.error(err.message);
             return res.status(500).send("Server Error");
